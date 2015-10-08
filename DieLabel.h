7a19ea89adef7404a5ee6d6525fd3447db235615
Michelle Burke <michele.burke01@gmail.com>
2015-10-08 16:26:43 -0500

    first commit

---
+++ DieLabel.h
@@ -0,0 +1,13 @@
+//
+//  DieLabel.h
+//  Farkle
+//
+//  Created by Michelle Burke on 10/8/15.
+//  Copyright © 2015 BurkeMedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DieLabel : UILabel
+
+@end
